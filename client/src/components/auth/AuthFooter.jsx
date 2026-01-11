52c20a9bf6537306b0cb65d9a032cc3c93df984d
MajdAwad12 <majdawad86@gmail.com>
2026-01-11 10:02:25 +0200

    Delete client directory

--- client/src/components/auth/AuthFooter.jsx
+++
@@ -1,7 +0,0 @@
-export default function AuthFooter() {
-  return (
-    <footer className="mt-6 text-center text-xs text-indigo-100">
-      Braude College • Web Course – Exam Monitoring Project • Team: Majd, Ali, Bashar, Aya
-    </footer>
-  );
-}
